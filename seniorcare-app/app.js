babe2246532db5caf1d3fb41f8790684af93abe5
danielapuerta <danielapuerta151@gmail.com>
2022-06-26 16:56:43 +0100

    install node-url-shortener

--- seniorcare-app/app.js
+++ seniorcare-app/app.js
@@ -1,10 +1,28 @@
+//require
 const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const bodyParser = require('body-parser');
+const urlShortener = require('node-url-shortener');
 
+//use
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded());
+
+//routes
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.post('/url', function(req, res) {
+    const url = req.body.url;
+
+    urlShortener.short(url, function(err, shortUrl){
+        res.send(shortUrl);
+    });
+});
+
+//listen
 app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
+
